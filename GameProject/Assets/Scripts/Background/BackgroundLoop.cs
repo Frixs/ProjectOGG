ede61c5c2ca94072952e3f10c48106806a0fa29e
Jakub Váverka <jakub.vaverka@email.cz>
2020-04-12 16:47:40 +0200

    Paralax fix

--- GameProject/Assets/Scripts/Background/BackgroundLoop.cs
+++ GameProject/Assets/Scripts/Background/BackgroundLoop.cs
@@ -7,8 +7,11 @@ public class BackgroundLoop : MonoBehaviour
     public GameObject[] levels;
     public Camera mainCamera;
     private Vector2 screenBounds;
+    private float position;
+    private float lastPositions;
     public float[] choke;
     public float[] scrollSpeed;
+    public bool movementParalax;
 
     void Start()
     {
@@ -20,8 +23,7 @@ public class BackgroundLoop : MonoBehaviour
     }
     void loadChildObjects(int obj)
     {
-        float objectWidth = levels[obj].GetComponent<SpriteRenderer>().bounds.size.x;
-        int childsNeeded = 4;
+        int childsNeeded = 7;
         GameObject clone = Instantiate(levels[obj]) as GameObject;
         for (int i = 0; i <= childsNeeded; i++)
         {
@@ -38,29 +40,42 @@ public class BackgroundLoop : MonoBehaviour
         Transform[] children = levels[obj].GetComponentsInChildren<Transform>();
         if (children.Length > 1)
         {
+            Debug.Log(position);
             GameObject firstChild = children[1].gameObject;
             GameObject lastChild = children[children.Length - 1].gameObject;
-            if (screenBounds.x*2 > lastChild.transform.position.x)
+            if (lastChild.transform.position.x < position + screenBounds.x)
             {
                 firstChild.transform.SetAsLastSibling();
                 firstChild.transform.position = new Vector3(lastChild.transform.position.x + choke[obj], lastChild.transform.position.y, lastChild.transform.position.z);
             }
-            else if (-screenBounds.x < firstChild.transform.position.x)
+            else if (firstChild.transform.position.x > position - screenBounds.x/2)
             {
                 lastChild.transform.SetAsFirstSibling();
                 lastChild.transform.position = new Vector3(firstChild.transform.position.x - choke[obj], firstChild.transform.position.y, firstChild.transform.position.z);
             }
         }
     }
+   
     void Update()
     {
+        lastPositions = position;
+        position = mainCamera.transform.position.x - screenBounds.x/2;
         for (int i = 0; i < levels.Length; i++)
         {
             Vector3 velocity = Vector3.zero;
-            Vector3 desiredPosition = levels[i].transform.position + new Vector3(scrollSpeed[i], 0, 0);
-            Vector3 smoothPosition = Vector3.SmoothDamp(levels[i].transform.position, desiredPosition, ref velocity, 0.3f);
+            Vector3 desiredPosition = levels[i].transform.position;
+            if (movementParalax)
+            {
+                desiredPosition.x += position * scrollSpeed[i] -lastPositions * scrollSpeed[i];
+                levels[i].transform.position = desiredPosition;
+            }
+            else 
+            {
+                desiredPosition += new Vector3(scrollSpeed[i], 0, 0);
+                Vector3 smoothPosition = Vector3.SmoothDamp(levels[i].transform.position, desiredPosition, ref velocity, 0.3f);
+                levels[i].transform.position = smoothPosition;
+            }
 
-            levels[i].transform.position = smoothPosition;
         }
 
     }
